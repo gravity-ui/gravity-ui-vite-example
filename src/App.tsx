d3abb1340668db4f280699f78d9148b53d8c5a47
Angelina Gadzhieva <lunory@yandex-team.ru>
2024-10-18 23:04:21 +0300

    feat: add AsideHeader to example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import {AsideHeader} from '@gravity-ui/navigation';
+import {Ghost} from '@gravity-ui/icons';
+
 import {InfoButtons} from './components/InfoButtons';
 import {Wrapper} from './components/Wrapper';
 
 const App = () => {
     return (
-        <Wrapper>
-            <InfoButtons />
-        </Wrapper>
+        <AsideHeader
+            logo={{icon: Ghost, text: 'vite-example'}}
+            compact={true}
+            hideCollapseButton={true}
+            renderContent={() => (
+                <Wrapper>
+                    <InfoButtons />
+                </Wrapper>
+            )}
+        />
     );
 };
 
