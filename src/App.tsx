f006a519b11f35200f1b0ef243d56c800f177ddd
Elena Makarova <el-makarova@yandex-team.ru>
2025-04-14 09:58:32 +0300

    fix: ThemeProveder should wrap all application

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
+import React from 'react';
+
 import {AsideHeader} from '@gravity-ui/navigation';
 import {Ghost} from '@gravity-ui/icons';
+import {Theme, ThemeProvider} from '@gravity-ui/uikit';
 
 import {InfoButtons} from './components/InfoButtons';
 import {Wrapper} from './components/Wrapper';
+import {DEFAULT_THEME} from './constants';
 
 const App = () => {
+    const [theme, setTheme] = React.useState<Theme>(DEFAULT_THEME);
+
     return (
-        <AsideHeader
-            logo={{icon: Ghost, text: 'vite-example'}}
-            compact={true}
-            hideCollapseButton={true}
-            renderContent={() => (
-                <Wrapper>
-                    <InfoButtons />
-                </Wrapper>
-            )}
-        />
+        <ThemeProvider theme={theme}>
+            <AsideHeader
+                logo={{icon: Ghost, text: 'vite-example'}}
+                compact={true}
+                hideCollapseButton={true}
+                renderContent={() => (
+                    <Wrapper setTheme={setTheme}>
+                        <InfoButtons />
+                    </Wrapper>
+                )}
+            />
+        </ThemeProvider>
     );
 };
 
